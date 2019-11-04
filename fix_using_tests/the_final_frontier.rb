8d2ac76dcd338c1e84758f5a031ec62936d78200
Alexis Chilinski <chilinski.a@gmail.com>
2019-11-04 23:10:44 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -14,8 +14,7 @@ def crew
 end
 
 def greet_crew(crew)
-  crew.each{|crew_member| puts "Hello #{crew_member}."}
-  binding.pry
+  crew.map{|crew_member| puts "Hello #{crew_member}."}
 end
 
 def engage
